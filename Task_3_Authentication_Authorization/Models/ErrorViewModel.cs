0726f51c9abe048a8a2eaba24044471eb253c5be
Sardorbecakhmedov <sardorbe2015@gmail.com>
2023-05-25 16:58:24 +0500

    .

--- Task_3_Authentication_Authorization/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Task_3_Authentication_Authorization.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
